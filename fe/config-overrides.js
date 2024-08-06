6de95da5a3c22eb60c57b8adf199f59c417f29f9
shivilSR4 <shivilkravi>
2024-08-06 21:46:11 +0530

    test commit4

--- fe/config-overrides.js
+++ fe/config-overrides.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = function override(config, env) {
-  config.output = {
-    ...config.output,
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'static/js/[name].[contenthash:8].js',
-    chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
-  };
+  if (env === 'production') {
+    config.output = {
+      ...config.output,
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'static/js/[name].[contenthash:8].js',
+      chunkFilename: 'static/js/[name].[contenthash:8].chunk.js',
+    };
+  }
 
   return config;
 };
